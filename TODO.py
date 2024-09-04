354e3afe0761aa9f245c12c31237f870fa68ef73
connor-meeks <connor.meeks@stryker.com>
2024-09-04 16:34:56 +0000

    Adding TODOs

---
+++ TODO.py
@@ -0,0 +1,5 @@
+# TODO: pull in branch name
+# TODO: flag addons with different color
+# TODO: dynamic api request based on which date/branches you select
+# TODO: paginated loop to get all results
+# TODO: inital page load for api selections, then run full api (performance)
