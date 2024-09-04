df5f94902cd18f2d1412ae36ff0578c87ce22e73
connor-meeks <connor.meeks@stryker.com>
2024-09-04 19:02:12 +0000

    Todo update

--- TODO.py
+++ TODO.py
@@ -1,5 +1,6 @@
 # TODO: pull in branch name
-# TODO: flag addons with different color
 # TODO: dynamic api request based on which date/branches you select
+# TODO: pull in correct kit name (as appears in webops)
+# TODO: flag addons with different color
 # TODO: paginated loop to get all results
 # TODO: inital page load for api selections, then run full api (performance)
