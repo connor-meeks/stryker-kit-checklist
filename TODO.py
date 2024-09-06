58ec4547b5403ff84cbf54f7a5c8673c26024d14
connor-meeks <connor.meeks@stryker.com>
2024-09-06 20:48:39 +0000

    Todo Update

--- TODO.py
+++ TODO.py
@@ -1,4 +1,1 @@
 # TODO: add custom kits to df_results (ex. case id 24608188)
-# TODO: flag addons with different color
-# TODO: paginated loop to get all results
-# TODO: handle 100+ kitIds in kit request
